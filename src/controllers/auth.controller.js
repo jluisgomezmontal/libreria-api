9e082939f068bd235e4048d89e4894bc3827b672
KIKE <you@example.com>
2025-10-02 13:34:19 -0600

    setup fixed

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -13,7 +13,6 @@ export const register = async (req, res) => {
 
     const user = await User.create({ name, email, password });
 
-    // 3️⃣ Responder con éxito
     res.status(201).json({ id: user._id, email: user.email });
   } catch (err) {
     res.status(400).json({ message: err.message });
