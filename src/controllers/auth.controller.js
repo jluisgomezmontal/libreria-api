d1e31624c3c1b0d1c87b310ec7c4f42ce1a250c9
KIKE <you@example.com>
2025-10-02 11:11:14 -0600

    name added

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -5,7 +5,7 @@ import jwt from "jsonwebtoken";
 export const register = async (req, res) => {
   const { name, email, password } = req.body;
   try {
-    const user = await User.create({ email, email, password });
+    const user = await User.create({ name, email, password });
     res.status(201).json({ id: user._id, email: user.email });
   } catch (err) {
     res.status(400).json({ message: err.message });
